34c4dd8cf99a6c3db047f432c048a04de2aab577
jamesshapiro <jamesshapiro@gmail.com>
2013-09-17 05:03:07 -0700

    Update JuzzleBoard.java
    Similar to my comment on the AlphabetJuzzleBoard.
    
    Changed line 27 from "int k = i * j + j;" to "int k = i * c + j;"
    
    In this case we have a 4x4 grid. Consider what happens on the second iteration of the loop on line 25. In this case we want the outcome on line 28 to be:
    
    board[1][0] = LOWERCASE.getIndex(letters[4]);
    
    But under the equation on line 27, we get int k = i * j + j = (1 * 0) + 0 = 0. This leaves us with:
    
    board[1][0] = LOWERCASE.getIndex(letters[0]);
    
    Other values are similarly miscalculated. The new equation, "int k = i * c + j;" fixes this problem. I'm assuming it's what the author originally intended.

--- TrieDictionary/src/net/webeggs/juzzle/board/JuzzleBoard.java
+++ TrieDictionary/src/net/webeggs/juzzle/board/JuzzleBoard.java
@@ -24,7 +24,7 @@ public class JuzzleBoard extends AbstractJuzzleBoard<Vocabulary>{
 		board = new char[r][c];
 		for (int i = 0; i < r; i++) {
 			for (int j = 0; j < c; j++) {
-				int k = i * j + j;
+				int k = i * c + j;
 				board[i][j] = letters[k];
 			}
 		}
